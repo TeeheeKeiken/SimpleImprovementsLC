f7bdf2d67501611da1bf682b01490cca5624ea16
TeeheeKeiken <teeheelp@gmx.net>
2022-04-03 19:51:05 +0200

    Added current branch to Versions.txt

--- buildAndDeploy/build.bat
+++ buildAndDeploy/build.bat
@@ -5,9 +5,11 @@ rmdir bin\SimpleImprovements.c4d /s /q
 del bin /s /q
 xcopy SimpleImprovements.c4d bin\SimpleImprovements.c4d /i /s /e /y /v
 
-REM Set Version.txt to last commit
-for /F "tokens=*" %%a in ('git log -1 --format^="%%ad-%%H" --date^=iso-strict') do set commit=%%a
-echo devBuild_%commit% >> bin\SimpleImprovements.c4d\Version.txt
+REM Set Version.txt to current branch name and last commit date and hash
+for /F "tokens=*" %%a in ('git log -1 --format^="%%ad_%%H" --date^=iso-strict') do set commit=%%a
+for /F "tokens=*" %%a in ('git rev-parse --abbrev-ref HEAD') do set branch=%%a
+
+echo dev_%branch%_%commit% >> bin\SimpleImprovements.c4d\Version.txt
 
 REM Pack everything
 set /p c4group=<buildAndDeploy\c4group.path
