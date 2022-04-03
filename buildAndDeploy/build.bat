f61c4c6d4dc8c0bff4911c255c1cac4bf95c13be
TeeheeKeiken <teeheelp@gmx.net>
2022-04-03 20:09:41 +0200

    Version.txt now contains info if the build was built with local changes.

--- buildAndDeploy/build.bat
+++ buildAndDeploy/build.bat
@@ -5,11 +5,16 @@ rmdir bin\SimpleImprovements.c4d /s /q
 del bin /s /q
 xcopy SimpleImprovements.c4d bin\SimpleImprovements.c4d /i /s /e /y /v
 
-REM Set Version.txt to current branch name and last commit date and hash
+REM Set Version.txt to current branch name, if there are local changes, and the last commit date and hash
 for /F "tokens=*" %%a in ('git log -1 --format^="%%ad_%%H" --date^=iso-strict') do set commit=%%a
 for /F "tokens=*" %%a in ('git rev-parse --abbrev-ref HEAD') do set branch=%%a
-
-echo dev_%branch%_%commit% >> bin\SimpleImprovements.c4d\Version.txt
+REM Disable echo for a bit so the output stays readable
+echo off
+for /F "tokens=*" %%a in ('git diff') do set diff=%%a
+set changesType=_local_
+if "%diff%"=="" changesType=
+echo on
+echo dev_%branch%%changesType%%commit% >> bin\SimpleImprovements.c4d\Version.txt
 
 REM Pack everything
 set /p c4group=<buildAndDeploy\c4group.path
