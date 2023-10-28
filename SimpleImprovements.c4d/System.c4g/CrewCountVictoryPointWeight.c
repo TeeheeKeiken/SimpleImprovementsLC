474d767a98fe221e0d8103832302336611cb52c5
TeeheeKeiken <teeheelp@gmx.net>
2023-10-29 01:39:23 +0200

    Proof of concept CrewCountVictoryPointWeight

---
+++ SimpleImprovements.c4d/System.c4g/CrewCountVictoryPointWeight.c
@@ -0,0 +1,22 @@
+/*-- Crew Count Victory Point Weight --*/
+
+#strict 3
+#appendto VICP
+
+local CrewCountWeight;
+
+public func Initialize()
+{
+	CrewCountWeight = 5;
+
+	return (inherited());
+}
+
+public func CalculateVictoryPointsForPlayer(int playerId)
+{
+	var playerVictoryPoints = inherited(playerId);
+
+	var playerCrewCount = GetCrewCount(playerId);
+	playerVictoryPoints += playerCrewCount / CrewCountWeight;
+	return playerVictoryPoints;
+}
