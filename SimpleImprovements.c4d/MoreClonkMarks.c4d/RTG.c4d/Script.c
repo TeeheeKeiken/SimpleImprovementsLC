bde94e9e7ac4606de9154fecfb3c560d5f5338a8
TeeheeKeiken <teeheelp@gmx.net>
2023-11-11 16:25:43 +0100

    Added controls to view energy production rate of RTG

--- SimpleImprovements.c4d/MoreClonkMarks.c4d/RTG.c4d/Script.c
+++ SimpleImprovements.c4d/MoreClonkMarks.c4d/RTG.c4d/Script.c
@@ -6,6 +6,15 @@
 #include B_50
 #include DACT // Damage control
 
+local _energyProduction;
+
+protected func Initialize()
+{
+	_energyProduction = 10;
+
+	_inherited();
+}
+
 // Maximum amount of power that can be stored in this power generator.
 public func GetCapacity() 
 {
@@ -20,6 +29,25 @@ public func GetGeneratorPriority()
 
 private func ProducePower()
 {
-	DoPower(10);
+	DoPower(_energyProduction);
 	DebugLog("Energy: %d", GetPower());
+}
+
+/* Controls */
+
+protected func ControlUp()
+{
+	[$TxtShowEnergy$|Image=CXEC]
+	ShowEnergyMessage();
+}
+
+protected func ContextShowEnergy()
+{
+	[$TxtShowEnergy$|Image=CXEC]
+	ShowEnergyMessage();
+}
+
+private func ShowEnergyMessage()
+{
+	Message("$TxtShowEnergyMsg$", this, _energyProduction);
 }
