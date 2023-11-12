10717a02936ead5cb9c8266c940e767b9b1c81e9
TeeheeKeiken <teeheelp@gmx.net>
2023-11-12 04:50:35 +0100

    Added secondary and tertiary resources to MoreClonkMars resource display

--- SimpleImprovements.c4d/MoreClonkMars.c4d/System.c4g/AppendToResourceInfoCollectible.c
+++ SimpleImprovements.c4d/MoreClonkMars.c4d/System.c4g/AppendToResourceInfoCollectible.c
@@ -1,9 +1,27 @@
 /*-- Resource Info Collectible --*/
 
 #strict 3
+
+/* Primary Resources */
 #appendto METL
 #appendto PSTC
 #appendto CRYS
+
+/* Secondary Resources */
+#appendto ORE1
+#appendto ROCK
+#appendto ERTH
+#appendto ASHS
+
+/* Cells */
+#appendto CELL
+#appendto FCLL
+
+/* Kits & Co. */
+#appendto CNKT
+#appendto SPCT
+#appendto PIKT
+#appendto WIRO
 
 public func ISB_IS_IsResource()
 {
